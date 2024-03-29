1bf650d995370820144b4a93c9e63bc9c32ad3bf
weixu <xuwei19850423@gmail.com>
2024-03-30 00:30:03 +1100

    layout carDetails

---
+++ app/components/CarDetails.tsx
@@ -0,0 +1,13 @@
+import './CarDetails.css'; // Import the CSS file
+
+export default function CarDetails() {
+    return (
+        <div className="flex justify-between items-center">
+            <div></div>
+            <div className="flex items-left h-full">
+                details
+            </div>
+            <div></div>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Banner from "./components/Banner";
+import CarDetails from "./components/CarDetails";
 
 
 export default function Home() {
   return (
-   <Banner />
+    <div>
+      <Banner />
+      <CarDetails />
+    </div>
   );
 }
