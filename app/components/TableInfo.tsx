ce43e89bd9c334a8a04cbf1c5636fc7244d0cc0d
weixu <xuwei19850423@gmail.com>
2024-03-30 18:48:50 +1100

    Adding TableInfo component

--- app/components/TableInfo.tsx
+++ app/components/TableInfo.tsx
@@ -1,43 +1,76 @@
-
-
+import './TableInfo.css';
 export default function TableInfo() {
     return (
-        <div className="flex items-left h-full">
-            TableInfo
-        <table>
-            <thead>
-                <tr>
-                    <th>Make</th> 
-                    <th>Model</th> 
-                    <th>Year</th> 
-                    <th style={{width:'225px'}}>Description</th>
-                    <th>Odometer</th>
-                    <th>Vehicle condition</th> 
-                    <th>Sale location</th> 
-                    <th>Sale category</th> 
-                    <th>Salvage Vehicle</th>
-                    <th>Sale date</th> 
-                    <th></th>
-                    <th>Sale price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Audi</td>
-                    <td>A5</td>
-                    <td>2016</td>
-                    <td>8T MY16 S line plus Coupe 2dr S tronic 7sp quattro 2.0T</td>
-                    <td>82,640</td>
-                    <td>Above Average</td>
-                    <td>Southport (QLD)</td>
-                    <td>Dealership</td>
-                    <td>No</td>
-                    <td>Mar 2024</td>
-                    <td><a>More</a></td>
-                    <td><button>Subscribe to reveal price</button></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+        <div className="h-full mx-auto px-8">
+            <div className="flex justify-between">
+                <div>
+                    <div>
+                        <strong> <span>Audi</span> <span>A5</span> <span>2016</span></strong>
+                    </div>
+                    <div>
+                        8T MY16 S line plus Coupe 2dr S tronic 7sp quattro 2.0T
+                    </div>
+                </div>
+
+                <div>
+                    <button className="subscribeButton">Subscribe to reveal price</button>
+                </div>
+            </div>
+
+            <div>
+                <div className="flex justify-left items-center">
+                    <div className="flex items-center mr-5">
+                        <div className="mr-1">
+                            <div className="h-1 w-1 rounded-full bg-black"></div>
+                        </div>
+                        <div>82,640</div>
+                    </div>
+                    <div className="flex items-center mr-5">
+                        <div className="mr-1">
+                            <div className="h-1 w-1 rounded-full bg-black"></div>
+                        </div>
+                        <div>Above Average</div>
+                    </div>
+
+                    <div className="flex items-center mr-5">
+                        <div className="mr-1">
+                            <div className="h-1 w-1 rounded-full bg-black"></div>
+                        </div>
+                        <div>Southport (QLD)</div>
+                    </div>
+
+                    <div className="flex items-center mr-5">
+                        <div className="mr-1">
+                            <div className="h-1 w-1 rounded-full bg-black"></div>
+                        </div>
+                        <div>Dealership</div>
+                    </div>
+
+                    <div className="flex items-center mr-5">
+                        <div className="mr-1">
+                            <div className="h-1 w-1 rounded-full bg-black"></div>
+                        </div>
+                        <div>No</div>
+                    </div>
+
+                    <div className="flex items-center mr-5">
+                        <div className="mr-1">
+                            <div className="h-1 w-1 rounded-full bg-black"></div>
+                        </div>
+                        <div>Mar 2024</div>
+                    </div>
+
+                    <div className="flex items-center mr-5">
+                        <div className="mr-1">
+                            <div className="h-1 w-1 rounded-full bg-black"></div>
+                        </div>
+                        <div>  <a>More</a></div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
     );
+
 }
