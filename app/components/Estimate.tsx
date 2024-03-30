a4416e6b7f122319932d84ac04bb9591cf884990
weixu <xuwei19850423@gmail.com>
2024-03-30 11:35:06 +1100

    Completed estimate Component layout

---
+++ app/components/Estimate.tsx
@@ -0,0 +1,20 @@
+import './Estimate.css'; // Import the CSS file
+
+export default function Estimate() {
+    return (
+        <div className='estimateContainor'>
+            <div className="flex flex-row">
+            <div className="w-1/5 none-730"></div>
+            <div className="w-4/5 ml-5 full-730">
+                    <div className="flex flex-col h-full ">
+                        <div className='estimateTitle estimateTitle-730'>Looking for a car estimate?</div>
+                        <div className='subEstimateTitle mt-3 subEstimateTitle-730'>Try our free valuation tool for an estimated value of your car.</div>
+                        <div>
+                            <button className='estimateButton'>Get free estimate</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
