9c8255db586c49e961453469e3adbb9cef85ad60
weixu <xuwei19850423@gmail.com>
2024-03-31 11:55:49 +1100

    acquire parameters from request URL

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,15 +1,22 @@
+'use client'
 import Banner from "../components/Banner";
 import CarDetails from "../components/CarDetails";
 import Estimate from "../components/Estimate";
 import Foot from "../components/Foot";
 import LoadMore from "../components/LoadMore";
-
+import { useSearchParams } from 'next/navigation'
 
 
 export default function Home() {
+  const searchParams = useSearchParams()
+ 
+  const make = searchParams.get('make') || 'Audi';
+  const model = searchParams.get('model') || 'A5';
+
   return (
+    
     <div>
-      <Banner />
+      <Banner  make={make} mode={model} />
       <CarDetails />
       <LoadMore />
       <Estimate />
