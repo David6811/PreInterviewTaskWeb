217ea8a52e52398957e4943066591fd10b2377fe
weixu <xuwei19850423@gmail.com>
2024-04-01 23:54:58 +1100

    show days in saleDate

--- app/action/UsedCarSalesUtil.tsx
+++ app/action/UsedCarSalesUtil.tsx
@@ -1,8 +1,8 @@
-
 export function formatSaleDateDate(saleDate: string): string {
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   const date = new Date(saleDate);
+  const day = date.getDate();
   const month = months[date.getMonth()];
   const year = date.getFullYear();
-  return `${month} ${year}`;
+  return `${day} ${month} ${year}`;
 }
